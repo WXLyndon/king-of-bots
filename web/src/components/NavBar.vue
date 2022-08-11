e48470edcc7bdfc5b1a151d09af482e98d62f3b1
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-08-11 02:12:21 -0400

    Implement the navbar active feature by using useRoute and computed.

--- web/src/components/NavBar.vue
+++ web/src/components/NavBar.vue
@@ -1,35 +1,82 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <router-link class="navbar-brand" :to="{name: 'home'}"> King of Bots</router-link>
+      <router-link class="navbar-brand" :to="{ name: 'home' }">
+        King of Bots</router-link
+      >
       <div class="collapse navbar-collapse" id="navbarText">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" :to="{name: 'battle_index'}">Battle</router-link>
+            <router-link
+              :class="
+                route_name == 'battle_index' ? 'nav-link active' : 'nav-link'
+              "
+              aria-current="page"
+              :to="{ name: 'battle_index' }"
+              >Battle</router-link
+            >
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'record_index'}">Game Record</router-link>
+            <router-link
+              :class="
+                route_name == 'record_index' ? 'nav-link active' : 'nav-link'
+              "
+              :to="{ name: 'record_index' }"
+              >Game Record</router-link
+            >
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" :to="{name: 'ranking_index'}">Ranking</router-link>
+            <router-link
+              :class="
+                route_name == 'ranking_index' ? 'nav-link active' : 'nav-link'
+              "
+              :to="{ name: 'ranking_index' }"
+              >Ranking</router-link
+            >
           </li>
         </ul>
         <ul class="navbar-nav">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            User
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><router-link class="dropdown-item" :to="{name: 'user_bot_index'}">My bots</router-link></li>
-            <li><a class="dropdown-item" href="#">Logout</a></li>
-          </ul>
-        </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdownMenuLink"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              User
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li>
+                <router-link
+                  class="dropdown-item"
+                  :to="{ name: 'user_bot_index' }"
+                  >My bots</router-link
+                >
+              </li>
+              <li><a class="dropdown-item" href="#">Logout</a></li>
+            </ul>
+          </li>
         </ul>
       </div>
     </div>
   </nav>
 </template>
 
-<script></script>
+<script>
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+export default {
+  setup() {
+    const route = useRoute();
+    let route_name = computed(() => route.name);
+    return {
+      route_name,
+    };
+  },
+};
+</script>
 
 <style scoped></style>
