00f9510307936d10ff3b5e1d3964271edf31b28a
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-08-10 21:21:18 -0400

    Write a getBotInfo to test the restful between web and backend.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,8 +1,35 @@
 <template>
-  <div>Hello World</div>
-  <router-view/>
+  <div>Bot's nickame: {{ bot_name }}</div>
+  <div>Bot's rating: {{ bot_rating }}</div>
+  <router-view />
 </template>
 
-<style>
+<script>
+import $ from "jquery";
+import { ref } from "vue";
 
+export default {
+  name: "App",
+  setup: () => {
+    let bot_name = ref("");
+    let bot_rating = ref("");
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/pk/getbotinfo/",
+      type: "GET",
+      success: (resp) => {
+        bot_name.value = resp.name;
+        bot_rating.value = resp.rating;
+      },
+    });
+
+    return {
+      bot_name,
+      bot_rating,
+    };
+  },
+};
+</script>
+
+<style>
 </style>
