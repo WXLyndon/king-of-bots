6945b98b58a12ce0a9ad94e4469e4cb7cc154e87
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-08-12 23:17:34 -0400

    Change the website's icon and name.

--- web/src/App.vue
+++ web/src/App.vue
@@ -17,7 +17,7 @@ export default {
 
 <style>
 body {
-  background-image: url("@/assets/background.jpg");
+  background-image: url("@/assets/images/background.jpg");
   background-size: cover;
 }
 </style>
