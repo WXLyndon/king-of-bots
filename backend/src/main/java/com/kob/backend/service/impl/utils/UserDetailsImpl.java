acbcfbfba55bb8c1a4c9ef69e11a1b1a82d000f6
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-08-20 18:59:17 -0400

    Create the UserDetailsServiceImpl class which implements UserDetailsService to achieve the very basic user login feature.

--- backend/src/main/java/com/kob/backend/service/impl/utils/UserDetailsImpl.java
+++ backend/src/main/java/com/kob/backend/service/impl/utils/UserDetailsImpl.java
@@ -1,11 +1,17 @@
 package com.kob.backend.service.impl.utils;
 
 import com.kob.backend.pojo.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDetailsImpl implements UserDetails {
 
     private User user;
