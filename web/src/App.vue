ec01a89c4b6e4659400d678e7f39a90e5565550c
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-08-22 21:25:57 -0400

    Implement the InfoController.

--- web/src/App.vue
+++ web/src/App.vue
@@ -7,10 +7,44 @@
 import NavBar from "./components/NavBar.vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import $ from "jquery";
 
 export default {
   components: {
     NavBar,
+  },
+
+  setup() {
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/token/",
+      type: "POST",
+      data: {
+        username: "lyndon",
+        password: "950817",
+      },
+      success: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+
+    $.ajax({
+      url: "http://127.0.0.1:3000/user/account/info/",
+      type: "GET",
+      headers: {
+        Authorization:
+          "Bearer " +
+          "eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlMDg2MDBiYzUzMWE0M2ZkYTIzYjE4NWQzYTEzMTZiNyIsInN1YiI6IjIiLCJpc3MiOiJzZyIsImlhdCI6MTY2MTIxNzQzNSwiZXhwIjoxNjYyNDI3MDM1fQ._iHhBxtR_xMo-UMX4yIEcuiJL4YrizK8gBGgQdSMC9g",
+      },
+      success: (resp) => {
+        console.log(resp);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   },
 };
 </script>
