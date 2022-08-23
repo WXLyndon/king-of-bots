231b37f267bbd78b4f0e10131fe047019680183a
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-08-23 00:02:56 -0400

    Add the router for login and register.

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import BattleIndexView from "../views/battle/BattleIndexView.vue";
-import RecordIndexView from "../views/record/RecordIndexView.vue";
-import RankingIndexView from "../views/ranking/RankingIndexView.vue";
-import UserBotIndexView from "../views/user/bot/UserBotIndexView.vue";
-import NotFound from "../views/error/NotFound.vue";
+import BattleIndexView from "../views/battle/BattleIndexView";
+import RecordIndexView from "../views/record/RecordIndexView";
+import RankingIndexView from "../views/ranking/RankingIndexView";
+import UserBotIndexView from "../views/user/bot/UserBotIndexView";
+import NotFound from "../views/error/NotFound";
+import UserAccountLoginView from "../views/user/account/UserAccountLoginView";
+import UserAccountRegisterView from "../views/user/account/UserAccountRegisterView";
 
 const routes = [
   {
@@ -32,6 +34,16 @@ const routes = [
     path: "/user/bot/",
     name: "user_bot_index",
     component: UserBotIndexView,
+  },
+  {
+    path: "/user/account/login/",
+    name: "user_account_login",
+    component: UserAccountLoginView,
+  },
+  {
+    path: "/user/account/register/",
+    name: "user_account_register",
+    component: UserAccountRegisterView,
   },
   { path: "/404/", name: "not_found", component: NotFound },
   { path: "/:catchAll(.*)", redirect: "/404/" },
