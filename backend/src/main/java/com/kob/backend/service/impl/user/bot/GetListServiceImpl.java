bb2d8236d08431b37460ef24e9fe6317daa573bf
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-08-29 19:54:44 -0400

    Add @Service tag for GetListServiceImpl.

--- backend/src/main/java/com/kob/backend/service/impl/user/bot/GetListServiceImpl.java
+++ backend/src/main/java/com/kob/backend/service/impl/user/bot/GetListServiceImpl.java
@@ -9,9 +9,11 @@ import com.kob.backend.service.user.bot.GetListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class GetListServiceImpl implements GetListService {
     @Autowired
     private BotMapper botMapper;
