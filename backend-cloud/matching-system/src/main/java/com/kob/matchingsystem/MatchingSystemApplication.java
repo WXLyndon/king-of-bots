3372240e68fd94f84ca8867f64fef9e2797c920c
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-09-20 11:57:21 -0400

    Make the matching pool thread starts running each time when matching system application runs.

--- backend-cloud/matching-system/src/main/java/com/kob/matchingsystem/MatchingSystemApplication.java
+++ backend-cloud/matching-system/src/main/java/com/kob/matchingsystem/MatchingSystemApplication.java
@@ -1,5 +1,6 @@
 package com.kob.matchingsystem;
 
+import com.kob.matchingsystem.service.impl.MatchingServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MatchingSystemApplication {
     public static void main(String[] args) {
 
+        MatchingServiceImpl.getMatchingPool().start();
         SpringApplication.run(MatchingSystemApplication.class, args);
     }
 }
