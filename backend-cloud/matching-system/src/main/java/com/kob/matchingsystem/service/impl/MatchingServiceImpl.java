81e2f976051b6566fbef38329c9ab269f36e2a53
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-09-20 11:55:11 -0400

    Implement the MatchingServiceImpl in matching system.

--- backend-cloud/matching-system/src/main/java/com/kob/matchingsystem/service/impl/MatchingServiceImpl.java
+++ backend-cloud/matching-system/src/main/java/com/kob/matchingsystem/service/impl/MatchingServiceImpl.java
@@ -1,19 +1,28 @@
 package com.kob.matchingsystem.service.impl;
 
 import com.kob.matchingsystem.service.MatchingService;
+import com.kob.matchingsystem.service.impl.utils.MatchingPool;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MatchingServiceImpl implements MatchingService {
+    private final static MatchingPool matchingPool = new MatchingPool();
+
+    public static MatchingPool getMatchingPool() {
+        return matchingPool;
+    }
+
     @Override
     public String addPlayer(Integer userId, Integer rating) {
         System.out.println("add player: " + userId + " " + rating);
+        matchingPool.addPlayer(userId, rating);
         return "add player successfully";
     }
 
     @Override
     public String removePlayer(Integer userId) {
         System.out.println("remove player: " + userId);
+        matchingPool.removePlayer(userId);
         return "remove player successfully";
     }
 }
