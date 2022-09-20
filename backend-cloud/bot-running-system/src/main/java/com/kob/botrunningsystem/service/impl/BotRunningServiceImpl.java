3a3d09cd0b9040e0faa34b4c1c4a5715d9722663
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-09-20 18:44:29 -0400

    Create the BotPool (multithread class) for bot-running-system.

--- backend-cloud/bot-running-system/src/main/java/com/kob/botrunningsystem/service/impl/BotRunningServiceImpl.java
+++ backend-cloud/bot-running-system/src/main/java/com/kob/botrunningsystem/service/impl/BotRunningServiceImpl.java
@@ -1,13 +1,22 @@
 package com.kob.botrunningsystem.service.impl;
 
 import com.kob.botrunningsystem.service.BotRunningService;
+import com.kob.botrunningsystem.service.impl.utils.BotPool;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BotRunningServiceImpl implements BotRunningService {
+
+    private final static BotPool botPool = new BotPool();
+
+    public static BotPool getBotPool() {
+        return BotRunningServiceImpl.botPool;
+    }
+
     @Override
     public String addBot(Integer userId, String botCode, String gameInfo) {
         System.out.println("Add bot: " + userId + " " + botCode + " " + gameInfo);
+        botPool.addBot(userId, botCode, gameInfo);
         return "Add bot successfully.";
     }
 }
