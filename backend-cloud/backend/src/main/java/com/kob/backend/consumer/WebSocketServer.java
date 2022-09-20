d4faa1cebd6cd16b3eeb269e6208ccd852bdd464
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-09-19 22:25:52 -0400

    Move the backend project as a new module under backend-cloud.

---
+++ backend-cloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -0,0 +1,167 @@
+package com.kob.backend.consumer;
+
+import com.alibaba.fastjson.JSONObject;
+import com.kob.backend.consumer.utils.Game;
+import com.kob.backend.consumer.utils.JwtAuthentication;
+import com.kob.backend.mapper.RecordMapper;
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@Component
+@ServerEndpoint("/websocket/{token}")  // Do not end with '/'
+public class WebSocketServer {
+
+    private final static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
+    private final static CopyOnWriteArraySet<User> matchPool = new CopyOnWriteArraySet<>();
+    private User user;
+    private Session session = null;
+
+    private static UserMapper userMapper;
+    private static RecordMapper recordMapper;
+    private Game game = null;
+
+    @Autowired
+    public void setUserMapper(UserMapper userMapper) {
+        WebSocketServer.userMapper = userMapper;
+    }
+
+    public static RecordMapper getRecordMapper() {
+        return recordMapper;
+    }
+
+    @Autowired
+    public void setRecordMapper(RecordMapper recordMapper) {
+        WebSocketServer.recordMapper = recordMapper;
+    }
+
+    public static ConcurrentHashMap<Integer, WebSocketServer> getUsers() {
+        return users;
+    }
+
+    @OnOpen
+    public void onOpen(Session session, @PathParam("token") String token) throws IOException {
+        // Construct connection
+        this.session = session;
+        Integer userId = JwtAuthentication.getUserId(token);
+        this.user = userMapper.selectById(userId);
+
+        if (this.user != null) {
+            users.put(userId, this);
+        } else {
+            this.session.close();
+        }
+
+        System.out.println(users);
+        System.out.println("Connected!");
+    }
+
+    @OnClose
+    public void onClose() {
+        // Close connection
+        System.out.println("Disconnected!");
+        if (this.user != null) {
+            users.remove(this.user.getId());
+            matchPool.remove(this.user);
+        }
+    }
+
+    private void startMatching() {
+        System.out.println("Start Matching!");
+        matchPool.add(this.user);
+
+        while (matchPool.size() >= 2) {
+            Iterator<User> it = matchPool.iterator();
+            User a = it.next();
+            User b = it.next();
+            matchPool.remove(a);
+            matchPool.remove(b);
+
+            Game game = new Game(13, 14, 5, a.getId(), b.getId());
+            game.createMap();
+
+            users.get(a.getId()).game = game;
+            users.get(b.getId()).game = game;
+
+            game.start(); // Start a new game thread
+
+            JSONObject respGame = new JSONObject();
+            respGame.put("a_id", game.getPlayerA().getId());
+            respGame.put("a_sx", game.getPlayerA().getSx());
+            respGame.put("a_sy", game.getPlayerA().getSy());
+
+            respGame.put("b_id", game.getPlayerB().getId());
+            respGame.put("b_sx", game.getPlayerB().getSx());
+            respGame.put("b_sy", game.getPlayerB().getSy());
+            respGame.put("map", game.getMap());
+
+
+            JSONObject respA = new JSONObject();
+            respA.put("event", "start-matching");
+            respA.put("opponent_username", b.getUsername());
+            respA.put("opponent_photo", b.getPhoto());
+            respA.put("game", respGame);
+            users.get(a.getId()).sendMessage(respA.toJSONString());
+
+            JSONObject respB = new JSONObject();
+            respB.put("event", "start-matching");
+            respB.put("opponent_username", a.getUsername());
+            respB.put("opponent_photo", a.getPhoto());
+            respB.put("game", respGame);
+            users.get(b.getId()).sendMessage(respB.toJSONString());
+        }
+    }
+
+    private void stopMatching() {
+        System.out.println("Stop Matching!");
+        matchPool.remove(this.user);
+    }
+
+    private void move(int direction) {
+        if (game.getPlayerA().getId().equals(user.getId())){
+            game.setNextStepA(direction);
+        } else if (game.getPlayerB().getId().equals(user.getId())) {
+            game.setNextStepB(direction);
+        }
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) { // Just like router
+        // Get message from clients
+        System.out.println("Message received!");
+        JSONObject data = JSONObject.parseObject(message);
+        String event = data.getString("event");
+
+        if ("start-matching".equals(event)) {
+            startMatching();
+        } else if ("stop-matching".equals(event)) {
+            stopMatching();
+        } else if ("move".equals(event)) {
+            move(data.getInteger("direction"));
+        }
+    }
+
+    @OnError
+    public void onError(Session session, Throwable error) {
+        error.printStackTrace();
+    }
+
+    public void sendMessage(String message) {
+        synchronized (this.session) {
+            try {
+                this.session.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
