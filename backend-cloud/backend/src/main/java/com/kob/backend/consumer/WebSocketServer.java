09559dce98cc118fa80bb4b4520bbf5eb2fe5e85
Xuanlin Wang <xuanlin.wang@mail.utoronto.ca>
2022-09-19 22:46:28 -0400

    Remove matchPool in WebSocketServer, implement helper method startGame.

--- backend-cloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backend-cloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -21,8 +21,10 @@ import java.util.concurrent.CopyOnWriteArraySet;
 @ServerEndpoint("/websocket/{token}")  // Do not end with '/'
 public class WebSocketServer {
 
+    private final static int rows = 13;
+    private final static int cols = 14;
+    private final static int innerWallsCount = 20;
     private final static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
-    private final static CopyOnWriteArraySet<User> matchPool = new CopyOnWriteArraySet<>();
     private User user;
     private Session session = null;
 
@@ -71,59 +73,56 @@ public class WebSocketServer {
         System.out.println("Disconnected!");
         if (this.user != null) {
             users.remove(this.user.getId());
-            matchPool.remove(this.user);
         }
+    }
+
+    private void startGame(Integer aId, Integer bId){
+        User a = userMapper.selectById(aId);
+        User b = userMapper.selectById(bId);
+
+        Game game = new Game(rows, cols, innerWallsCount, a.getId(), b.getId());
+        game.createMap();
+
+        users.get(a.getId()).game = game;
+        users.get(b.getId()).game = game;
+
+        game.start(); // Start a new game thread
+
+        JSONObject respGame = new JSONObject();
+        respGame.put("a_id", game.getPlayerA().getId());
+        respGame.put("a_sx", game.getPlayerA().getSx());
+        respGame.put("a_sy", game.getPlayerA().getSy());
+
+        respGame.put("b_id", game.getPlayerB().getId());
+        respGame.put("b_sx", game.getPlayerB().getSx());
+        respGame.put("b_sy", game.getPlayerB().getSy());
+        respGame.put("map", game.getMap());
+
+
+        JSONObject respA = new JSONObject();
+        respA.put("event", "start-matching");
+        respA.put("opponent_username", b.getUsername());
+        respA.put("opponent_photo", b.getPhoto());
+        respA.put("game", respGame);
+        users.get(a.getId()).sendMessage(respA.toJSONString());
+
+        JSONObject respB = new JSONObject();
+        respB.put("event", "start-matching");
+        respB.put("opponent_username", a.getUsername());
+        respB.put("opponent_photo", a.getPhoto());
+        respB.put("game", respGame);
+        users.get(b.getId()).sendMessage(respB.toJSONString());
+
     }
 
     private void startMatching() {
         System.out.println("Start Matching!");
-        matchPool.add(this.user);
-
-        while (matchPool.size() >= 2) {
-            Iterator<User> it = matchPool.iterator();
-            User a = it.next();
-            User b = it.next();
-            matchPool.remove(a);
-            matchPool.remove(b);
-
-            Game game = new Game(13, 14, 5, a.getId(), b.getId());
-            game.createMap();
-
-            users.get(a.getId()).game = game;
-            users.get(b.getId()).game = game;
-
-            game.start(); // Start a new game thread
-
-            JSONObject respGame = new JSONObject();
-            respGame.put("a_id", game.getPlayerA().getId());
-            respGame.put("a_sx", game.getPlayerA().getSx());
-            respGame.put("a_sy", game.getPlayerA().getSy());
-
-            respGame.put("b_id", game.getPlayerB().getId());
-            respGame.put("b_sx", game.getPlayerB().getSx());
-            respGame.put("b_sy", game.getPlayerB().getSy());
-            respGame.put("map", game.getMap());
 
 
-            JSONObject respA = new JSONObject();
-            respA.put("event", "start-matching");
-            respA.put("opponent_username", b.getUsername());
-            respA.put("opponent_photo", b.getPhoto());
-            respA.put("game", respGame);
-            users.get(a.getId()).sendMessage(respA.toJSONString());
-
-            JSONObject respB = new JSONObject();
-            respB.put("event", "start-matching");
-            respB.put("opponent_username", a.getUsername());
-            respB.put("opponent_photo", a.getPhoto());
-            respB.put("game", respGame);
-            users.get(b.getId()).sendMessage(respB.toJSONString());
-        }
     }
 
     private void stopMatching() {
         System.out.println("Stop Matching!");
-        matchPool.remove(this.user);
     }
 
     private void move(int direction) {
